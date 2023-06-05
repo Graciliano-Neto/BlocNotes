fd9963066e363310bcd752ac4fda1495b4fa06f8
Graciliano-Neto <116576105+Graciliano-Neto@users.noreply.github.com>
2023-06-05 19:39:15 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import BlocForm from "./Components/BlocForm";
+import BlocList from "./Components/BlocList";
+
+function App (){
+    return (
+        <main>
+            <BlocForm />
+            <BlocList />
+        </main>
+    );
+}
+export default App;
---
+++ src/Data/Notes.js
@@ -0,0 +1,22 @@
+export const Notes = [
+    {
+        id: 0,
+        title: "Primeira anotação",
+        description: "Essa é minha primeira anotação de exemplo",
+    },
+    {
+        id: 1,
+        title: "Segunda anotação",
+        description: "Essa é minha segunda anotação de exemplo",
+    },
+    {
+        id: 2,
+        title: "Terceira anotação",
+        description: "Essa é minha terceira anotação de exemplo",
+    },
+    {
+        id: 3,
+        title: "Quarta anotação",
+        description: "Essa é minha quarta anotação de exemplo",
+    },
+];
